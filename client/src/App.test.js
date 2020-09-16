d1614d987b7e327b0fd67dc77d6f80fc0e5738aa
Michael Su <msu@atlassian.com>
2020-09-16 11:47:44 +1000

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// comment out because the test will crash, since the component will render Fib.
+it('renders without crashing', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
